59dfec5955330509d1092b51ff0b5032e733f250
rolandfbk <rolandfbk@gmail.com>
2023-01-26 10:55:05 +0200

    linters fixed

--- app/controllers/analytics_controller.rb
+++ app/controllers/analytics_controller.rb
@@ -2,7 +2,7 @@ class AnalyticsController < ApplicationController
   skip_before_action :verify_authenticity_token
 
   def index
-    @analytics = Analytic.all.group_by(&:search).map { |k, v| [k, v.count] }.to_h.sort_by { |k, v| v }.reverse
+    @analytics = Analytic.all.group_by(&:search).map { |k, v| [k, v.count] }.to_h.sort_by { |_k, v| v }.reverse
   end
 
   def show
@@ -35,8 +35,7 @@ class AnalyticsController < ApplicationController
 
   def user_search
     @user = User.find(params[:user_id])
-    @analytics = @user.analytics.group_by(&:search).map { |k, v| [k, v.count] }.to_h.sort_by { |k, v| v }.reverse
+    @analytics = @user.analytics.group_by(&:search).map { |k, v| [k, v.count] }.to_h.sort_by { |_k, v| v }.reverse
     render json: { analytics: @analytics }
   end
-
 end
